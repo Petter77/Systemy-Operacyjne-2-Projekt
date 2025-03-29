c61550e85d4a855ce793af5bffd83daf1d3c9732
petter77 <ppawlinski77@gmail.com>
2025-03-29 16:55:59 +0100

    Add check for number of call arguments and error messege

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello world!" << std::endl;
+int main(int argc, char **argv){
+    if(argc != 2){
+        std::cerr <<    "Error: Program requires one argument\n" 
+                        "Correct usage: ./MyProgram <number of philosophers>\n"; 
+        return 1;
+    }
+    return 0;
 }
